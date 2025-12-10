d0e39aad6e632586ab8c0370d0232d5f36bba806
challauday511 <udaykiran2312@gmail.com>
2025-12-10 16:21:00 +0530

    feature101 changes by ram

---
+++ application.java
@@ -0,0 +1,36 @@
+package com.webapp.bankingportal.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+#feture101 code changes by ram on dev 2025
+public class AccountResponse {
+
+    private String accountNumber;
+    private double balance;
+    private String accountType;
+    private String branch;
+    private String ifscCode;
+
+    public AccountResponse(Account account) {
+        this.accountNumber = account.getAccountNumber();
+        this.balance = account.getBalance();
+        this.accountType = account.getAccountType();
+        this.branch = account.getBranch();
+        this.ifscCode = account.getIfscCode();
+    }
+
+}
+#end of features101 changes
